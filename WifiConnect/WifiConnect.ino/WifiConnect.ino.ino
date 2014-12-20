16a1c644175809f1a92ed9e408c00d18e516cca2
Lance Johnson <lance.johnson@asynchrony.com>
2014-12-19 23:13:00 -0600

    Connect wifi board to sample upload server and post random temp and moisture. Lance

--- WifiConnect/WifiConnect.ino/WifiConnect.ino.ino
+++ WifiConnect/WifiConnect.ino/WifiConnect.ino.ino
@@ -1,47 +1,13 @@
-/****************************************************************
-WebClient.ino
-CC3000 WebClient Test
-Shawn Hymel @ SparkFun Electronics
-March 1, 2014
-https://github.com/sparkfun/SFE_CC3000_Library
-
-Manually connects to a WiFi network and performs an HTTP GET
-request on a web page. Prints the contents of the page to
-the serial console.
-
-The security mode is defined by one of the following:
-WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA, WLAN_SEC_WPA2
-
-Hardware Connections:
- 
- Uno Pin    CC3000 Board    Function
- 
- +5V        VCC or +5V      5V
- GND        GND             GND
- 2          INT             Interrupt
- 7          EN              WiFi Enable
- 10         CS              SPI Chip Select
- 11         MOSI            SPI MOSI
- 12         MISO            SPI MISO
- 13         SCK             SPI Clock
-
-Resources:
-Include SPI.h, SFE_CC3000.h, and SFE_CC3000_Client.h
-
-Development environment specifics:
-Written in Arduino 1.0.5
-Tested with Arduino UNO R3
-
-This code is beerware; if you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, please
-buy us a round!
-
-Distributed as-is; no warranty is given.
-****************************************************************/
-
 #include <SPI.h>
 #include <SFE_CC3000.h>
 #include <SFE_CC3000_Client.h>
+#include<stdlib.h>
+
+/*******************************************************
+ * Edit app_ssid & ap_password with your wifi ssid and password.
+ * Point browser at http://onedogdev.com/results.php
+ * When script sends data it should show up there.
+ *******************************************************/
 
 // Pins
 #define CC3000_INT      2   // Needs to be an interrupt pin (D2/D3)
@@ -52,101 +18,121 @@ Distributed as-is; no warranty is given.
 #define IP_ADDR_LEN     4   // Length of IP address in bytes
 
 // Constants
-char ap_ssid[] = "LJGuestNet";                  // SSID of network
-char ap_password[] = "beourguest";          // Password of network
+char ap_ssid[] = "ljmobile";                  // SSID of network
+char ap_password[] = "password";          // Password of network
+char server[] = "onedogdev.com";        // Remote host site
 unsigned int ap_security = WLAN_SEC_WPA2; // Security of network
 unsigned int timeout = 30000;             // Milliseconds
-char server[] = "onedogdev.com/garden.php";        // Remote host site
+unsigned int collectionDelay = 5000; // The amount of time to sleep between reading collection and post
 
 // Global Variables
 SFE_CC3000 wifi = SFE_CC3000(CC3000_INT, CC3000_EN, CC3000_CS);
 SFE_CC3000_Client client = SFE_CC3000_Client(wifi);
 
 void setup() {
-  
-  ConnectionInfo connection_info;
-  int i;
-  
   // Initialize Serial port
   Serial.begin(115200);
   Serial.println();
-  Serial.println("---------------------------");
-  Serial.println("SparkFun CC3000 - WebClient");
-  Serial.println("---------------------------");
+  Serial.println("-------------------------------");
+  Serial.println("Asynchrony Self Watering Garden");
+  Serial.println("-------------------------------");
   
-  // Initialize CC3000 (configure SPI communications)
-  if ( wifi.init() ) {
-    Serial.println("CC3000 initialization complete");
-  } else {
-    Serial.println("Something went wrong during CC3000 init!");
-  }
-  
-  // Connect using DHCP
-  Serial.print("Connecting to SSID: ");
-  Serial.println(ap_ssid);
-  if(!wifi.connect(ap_ssid, ap_security, ap_password, timeout)) {
-    Serial.println("Error: Could not connect to AP");
-  }
-  
-  // Gather connection details and print IP address
-  if ( !wifi.getConnectionInfo(connection_info) ) {
-    Serial.println("Error: Could not obtain connection details");
-  } else {
-    Serial.print("IP Address: ");
-    for (i = 0; i < IP_ADDR_LEN; i++) {
-      Serial.print(connection_info.ip_address[i]);
-      if ( i < IP_ADDR_LEN - 1 ) {
-        Serial.print(".");
-      }
+  randomSeed(analogRead(0)); // Read analog noise to seed randomizer
+}
+
+void loop() {
+  Serial.println("===================== Next Attempt =============");
+  if(initializeWifi()) {
+    if(connectToAccessPoint()) {
+      Serial.println("Collecting sensor data");
+      // TODO get data from sensors
+      float temp = random(100);
+      int moisture = random(300);
+      Serial.println("Sensor data collection complete");
+      
+      postValues(temp, moisture);
     }
-    Serial.println();
   }
-  
-//  float temp = 98.6;
-//  int moisture = 300;
-//  postValues(temp, mositure);
-  // Make a TCP connection to remote host
-  Serial.print("Performing HTTP GET of: ");
+  Serial.println("Waiting before next loop");
+  delay(collectionDelay);
+}
+
+boolean initializeWifi() {
+  if(!wifi.getInitStatus()) {
+    if ( wifi.init() ) {
+      Serial.println("WIFI initialization complete");
+    } else {
+      Serial.println("Something went wrong during WIFI init!");
+      return false;
+    }
+  } else {
+    Serial.println("WIFI already initialized and ready to go");
+  }
+  return true;
+}
+
+boolean connectToAccessPoint() {
+  if(!wifi.getConnectionStatus()) {
+    // Connect to access point using DHCP
+    Serial.print("Connecting to SSID: ");
+    Serial.println(ap_ssid);
+    if(!wifi.connect(ap_ssid, ap_security, ap_password, timeout)) {
+      Serial.println("Error: Could not connect to AP");
+      return false;
+    }
+    
+    ConnectionInfo connection_info;
+    int i;
+    // Gather connection details and print IP address
+    if ( !wifi.getConnectionInfo(connection_info) ) {
+      Serial.println("Error: Could not obtain connection details");
+      return false;
+    } else {
+      Serial.print("IP Address: ");
+      for (i = 0; i < IP_ADDR_LEN; i++) {
+        Serial.print(connection_info.ip_address[i]);
+        if ( i < IP_ADDR_LEN - 1 ) {
+          Serial.print(".");
+        }
+      }
+      Serial.println();
+    }
+  } else {
+      Serial.println("Already connected to AP");    
+  }
+  return true;
+}
+
+void postValues(float temp, int moisture) {
+  client.flush();
+
+  Serial.println("Connecting to server: ");
   Serial.println(server);
   if ( !client.connect(server, 80) ) {
-    Serial.println("Error: Could not make a TCP connection");
+    Serial.print("Error: Could not make a TCP connection to ");
+    Serial.println(server);
+    return;
+  } else {
+    Serial.print("Successfully connected to ");
+    Serial.println(server);
   }
   
-  // Make a HTTP GET request
-  client.println("GET /index.html HTTP/1.1");
+  char tempStr[10];
+  dtostrf(temp, 1, 2, tempStr);
+
+  String request = "/garden.php?temp=";
+  request += tempStr;
+  request += "&moisture=";
+  request += moisture;
+  request += " HTTP/1.1";
+  
+  Serial.println("Performing HTTP GET of: " + request);
+  client.println("GET " + request + " HTTP/1.1");
   client.print("Host: ");
   client.println(server);
   client.println("Connection: close");
   client.println();
-  Serial.println();
-}
-
-void loop() {
   
-  // If there are incoming bytes, print them
-  if ( client.available() ) {
-    char c = client.read();
-    Serial.print(c);
-  }
-  
-  // If the server has disconnected, stop the client and wifi
-  if ( !client.connected() ) {
-    Serial.println();
-    
-    // Close socket
-    if ( !client.close() ) {
-      Serial.println("Error: Could not close socket");
-    }
-    
-    // Disconnect WiFi
-    if ( !wifi.disconnect() ) {
-      Serial.println("Error: Could not disconnect from network");
-    }
-    
-    // Do nothing
-    Serial.println("Finished WebClient test");
-    while(true){
-      delay(1000);
-    }
-  }
+  client.flush();
+  Serial.println("Completed GET.");
 }
